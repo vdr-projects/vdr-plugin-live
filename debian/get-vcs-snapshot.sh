4c746f634d81d8120a4f099a80a5cfcaa33b62bf
etobi <git@e-tobi.net>
2014-10-29 22:50:28 +0100

    Updated get-vcs-snapshot.sh

--- debian/get-vcs-snapshot.sh
+++ debian/get-vcs-snapshot.sh
@@ -2,13 +2,14 @@ echo Downloading latest from git repository...
 set -e
 
 DEB_SOURCE_PACKAGE=`egrep '^Source: ' debian/control | cut -f 2 -d ' '`
+VERSION_UPSTREAM=`dpkg-parsechangelog | grep ^Version: | cut -d' ' -f2 | cut -d- -f1 | sed -r 's/(.*)\+git.*/\1/'`
 VERSION_DATE=`/bin/date --utc +%0Y%0m%0d`
-VERSION_FULL="0.2.0+git${VERSION_DATE}"
+VERSION_FULL="${VERSION_UPSTREAM}+git${VERSION_DATE}"
 
 git clone --depth 1 git://projects.vdr-developer.org/${DEB_SOURCE_PACKAGE}.git
 
 cd ${DEB_SOURCE_PACKAGE}
-GIT_SHA=`git show --pretty=format:"%h" --quiet || true`
+GIT_SHA=`git show --pretty=format:"%h" --quiet | head -1 || true`
 cd ..
 
 tar --exclude=.git -czf "../${DEB_SOURCE_PACKAGE}_${VERSION_FULL}.orig.tar.gz" ${DEB_SOURCE_PACKAGE}
